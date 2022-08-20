158a9d8f6398b0553523d73d261d75f9c0b8682d
rasitagac <rasitagac@hotmail.com>
2022-08-20 22:06:18 +0300

    Study at 20.08.2022

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import { createRoot } from "react-dom/client";
 import { faker } from "@faker-js/faker";
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
+import './style/App.css';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning?</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
 			<ApprovalCard>
 				<CommentDetail
 					author={faker.name.fullName()}
@@ -15,18 +22,22 @@ const App = () => {
 					avatar={faker.image.avatar()}
 				/>
 			</ApprovalCard>
-			<CommentDetail
-				author={faker.name.fullName()}
-				timeAgo="Today at 2:00AM"
-				content={faker.lorem.sentence()}
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author={faker.name.fullName()}
-				timeAgo="Yesterday at 10:45PM"
-				content={faker.lorem.sentence()}
-				avatar={faker.image.avatar()}
-			/>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.fullName()}
+          timeAgo="Today at 2:00AM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+			<ApprovalCard>
+        <CommentDetail
+          author={faker.name.fullName()}
+          timeAgo="Yesterday at 10:45PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
 		</div>
 	);
 };
